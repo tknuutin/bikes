65959e0915c8e3dd4b816854fa2ee8f80a72fd60
tknuutin <tarmo.knuutinen@gmail.com>
2014-01-06 22:10:53 -0500

    Made Bike a subclass of Shape

--- src/Bike.js
+++ src/Bike.js
@@ -1,19 +1,37 @@
 
-define(['src/Circle'], function(Circle){
+define(['src/Shape', 'src/Circle'], function(Shape, Circle){
     var Bike = function(opts){
         var self = this;
         opts = opts || {};
 
         var init = function(){
+
+            opts.x = 40;
+            opts.y = 10;
+
+            Shape.call(self, opts);
+
             self.leftTire = new Circle({
-                radius: 30,
-                x: 10, y: 10,
+                radius: 20,
+                x: -30, y: 10,
             });
 
             self.rightTire = new Circle({
-                radius: 30,
-                x: 70, y: 10
+                radius: 20,
+                x: 30, y: 10
             });
+
+            self.bikeBody = 
+
+            self.shapes = [self.leftTire, self.rightTire]
+        };
+
+        this.draw = function(ctx){
+            for (var i = 0, len = self.shapes.length; i < len; i++) {
+                self.shapes[i].predraw(ctx);
+                self.shapes[i].draw(ctx);
+                self.shapes[i].postdraw(ctx);
+            }
         };
 
         init();
--- src/Circle.js
+++ src/Circle.js
@@ -10,9 +10,11 @@ define(['src/Shape'], function(Shape){
         };
 
         this.draw = function(ctx){
+            ctx.beginPath();
             ctx.arc(self.radius, self.radius, self.radius, 0, Math.PI * 2, true);
             ctx.fillStyle = '#000';
             ctx.fill();
+            ctx.closePath();
         };
 
         init();
